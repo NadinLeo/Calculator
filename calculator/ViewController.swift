a2dc3871f40f28ef1e14ce0d18836904b791b854
Nadzeya <Nadezhda.Leonovich@gmail.com>
2019-08-24 13:42:18 +0300

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Nadzeya Leanovich on 8/24/19.
+//  Copyright © 2019 Nadzeya Leanovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
